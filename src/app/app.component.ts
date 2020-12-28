8544d5ff204f7ded0bf7b99b98ccb2ca19012f07
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-28 07:22:06 -0800

    Added pipes examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  inputDate: string;
+  amount: number;
+  
+  someObject = {
+    make: 'Ford',
+    year: 1999
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.inputDate = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
